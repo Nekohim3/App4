8379f4f951b09ea699ea789834abe015a5dc4dbc
Nekohime <kuroneko@protonmail.com>
2020-11-27 07:43:29 +0300

    Init

--- App4/App4.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ App4/App4.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"e4301218-e1a1-4195-9877-4d6c96f42637"
+	.asciz	"1367b735-6a67-448c-8766-188637704426"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
